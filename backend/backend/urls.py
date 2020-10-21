0525735142b3599f23f7c8f826be95a70053b162
Abhishek0706 <apatil@cs.iitr.ac.in>
2020-10-21 19:42:04 +0530

    added apps

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('feedback/', include("feedback.urls")),
+    path('menu/', include("menu.urls")),
+    path('user/', include("user.urls")),
+    path('expense/', include("expense.urls")),
+    path('wastage/', include("wastage.urls")),
 ]
+
+# superuser :
+# username = superuser
+# email = superuser@gmail.com
+# pass = superuser
--- backend/mess_management/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MessManagementConfig(AppConfig):
-    name = 'mess_management'
