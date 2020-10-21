73bd524702cfc67cc93f3e4b75a20d3c27aa0baa
Abhishek0706 <apatil@cs.iitr.ac.in>
2020-10-21 20:46:05 +0530

    test methods in feedback and disabled csrf tocken

--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def index(request):
+    if request.method == 'GET':
+        return HttpResponse("Feedback Home get request")
+    elif request.method == 'POST':
+        return HttpResponse(status=202)
+    else:
+        return HttpResponse(status=404)
