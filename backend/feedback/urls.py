0a175f066d4d86c80ed5e593aabcfb41ccac23b1
Abhishek0706 <apatil@cs.iitr.ac.in>
2020-10-22 21:15:23 +0530

    feedback get complete

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from feedback import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.get_feedback, name='get_feedback'),
+    path('add/', views.add_feedback, name='add_feedback'),
+    path('del/', views.del_feedback, name='del_feedback'),
 ]
