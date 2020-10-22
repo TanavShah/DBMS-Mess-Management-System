78c3c9a1eb749f4c8b905a81b51ca06d5e580dd0
Abhishek0706 <apatil@cs.iitr.ac.in>
2020-10-22 23:51:26 +0530

    added get method for student

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from user import views
+
+# 1. Add/update worker
+# 2. Add/update student
+# 5. View all students
+# 6. View all workers
+
 
 urlpatterns = [
-    ]
+    path('student/', views.get_student, name='get_student'),
+    path('student/add/', views.add_student, name='add_student'),
+    path('student/update/', views.update_student, name='update_student'),
+    path('worker/', views.get_worker, name='get_worker'),
+    path('worker/add/', views.add_worker, name='add_worker'),
+    path('worker/update/', views.update_worker, name='update_worker'),
+]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
+import json
+
 
 # Create your views here.
+def get_student(request):
+    if request.method != 'GET':
+        return HttpResponse(status=400)
+
+    hostel = request.GET.get('hostel', None)
+    enrollment_no = request.GET.get('enrollment_no', None)
+
+    if hostel is not None:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT public.student.enrollment_no , year_no, branch, email, full_name, phone_no, dateofbirth, bhawan
+            FROM public.student INNER JOIN public.userdata
+            ON public.student.enrollment_no = public.userdata.enrollment_no 
+            WHERE LOWER(bhawan) LIKE LOWER(%s) ;
+            """, (hostel,))
+
+            rows = cursor.fetchall()
+
+    elif enrollment_no is not None:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT public.student.enrollment_no , year_no, branch, email, full_name, phone_no, dateofbirth, bhawan
+            FROM public.student INNER JOIN public.userdata
+            ON public.student.enrollment_no = public.userdata.enrollment_no
+            WHERE public.student.enrollment_no = %s ;
+            """, (enrollment_no,))
+
+            rows = cursor.fetchall()
+
+    else:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT public.student.enrollment_no , year_no, branch, email, full_name, phone_no, dateofbirth, bhawan
+            FROM public.student INNER JOIN public.userdata
+            ON public.student.enrollment_no = public.userdata.enrollment_no
+            """)
+
+            rows = cursor.fetchall()
+
+    result = []
+    for row in rows:
+        temp = {'enrollment_no': row[0], 'year_no':row[1],'branch': row[2], 'email': row[3], 'full_name': row[4],
+                'phone_no': row[5], 'dateofbirth': str(row[6]), 'bhawan': row[7]}
+        result.append(temp)
+
+    json_data = json.dumps(result)
+    return HttpResponse(json_data, content_type="application/json")
+
+
+def add_student(request):
+    if request.method != 'POST':
+        return HttpResponse(status=400)
+    return None
+
+
+def update_student(request):
+    if request.method != 'POST':
+        return HttpResponse(status=400)
+    return None
+
+
+def get_worker(request):
+    if request.method != 'GET':
+        return HttpResponse(status=400)
+    return None
+
+
+def add_worker(request):
+    if request.method != 'POST':
+        return HttpResponse(status=400)
+    return None
+
+
+def update_worker(request):
+    if request.method != 'POST':
+        return HttpResponse(status=400)
+    return None
