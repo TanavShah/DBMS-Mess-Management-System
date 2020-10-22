6ef817e6490712cb01fb0ece718ae22760b08815
abhishek <apatil@cs.iitr.ac.in>
2020-10-22 11:05:09 +0530

    added postgres with localhost....create user with username= myuser and pass = mypass

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -26,7 +25,6 @@ SECRET_KEY = ')8!e$%xy1ji23x5&55uobppur#4i)y)fw5h3c+s(av0_bc9^q-'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,17 +73,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    # After we host postgres
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mess',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -105,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,7 +124,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpResponse
 
 
-# Create your views here.
+# Feedback Table is this
+# id | date_time | student_id | title | description
 
 
 def index(request):
     if request.method == 'GET':
-        return HttpResponse("Feedback Home get request")
+        return get_feedback(request)
     elif request.method == 'POST':
-        return HttpResponse(status=202)
+        return post_feedback(request)
     else:
         return HttpResponse(status=404)
+
+
+def get_feedback(request):
+    # http://127.0.0.1:8000/feedback/?hostel=rajiv
+    hostel = request.GET['hostel']
+    return HttpResponse("Feedback Home get request " + hostel)
+
+
+def post_feedback(request):
+    return HttpResponse(status=202)
