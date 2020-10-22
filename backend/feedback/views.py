d2f0a7caaa1f351d35fe6e83e43678d941675c3b
Abhishek0706 <apatil@cs.iitr.ac.in>
2020-10-22 22:25:56 +0530

    remove print statement and added infosec stuff

--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -9,25 +9,24 @@ import json
 # http://127.0.0.1:8000/feedback/?hostel=rajiv
 def get_feedback(request):
     hostel = request.GET.get('hostel', None)
-    print(hostel)
-    raw_query = ""
     if hostel is None:
-        raw_query = "SELECT * FROM public.feedback"
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM public.feedback")
+            rows = cursor.fetchall()
     else:
-        raw_query = "SELECT id, date_time, student_id, title, feedback_description " \
-                    "FROM public.feedback INNER JOIN public.userdata " \
-                    "ON public.feedback.student_id=public.userdata.enrollment_no " \
-                    "WHERE LOWER(bhawan) LIKE LOWER('%s')" % hostel
-    print(raw_query)
-    with connection.cursor() as cursor:
-        cursor.execute(raw_query)
-        rows = cursor.fetchall()
-        print(rows)
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT id, date_time, student_id, title, feedback_description 
+            FROM public.feedback INNER JOIN public.userdata 
+            ON public.feedback.student_id=public.userdata.enrollment_no 
+            WHERE LOWER(bhawan) LIKE LOWER(%s) ;
+            """, (hostel,))
+            rows = cursor.fetchall()
+
     result = []
     for row in rows:
         temp = {'id': row[0], 'date_time': str(row[1]), 'student_id': row[2], 'title': row[3],
                 'feedback_description': row[4]}
-        print(temp)
         result.append(temp)
 
     json_data = json.dumps(result)
@@ -38,12 +37,15 @@ def add_feedback(request):
     student_id = request.POST.get('student_id', '1111')
     title = request.POST.get('title', 'review')
     feedback_description = request.POST.get('feedback_description', 'not good')
-    raw_query = "INSERT INTO public.feedback (id , date_time, student_id, title, feedback_description) " \
-                "VALUES (DEFAULT, NOW()::TIMESTAMP(0), '%s', '%s', '%s')" % (student_id, title, feedback_description)
-
-    print(raw_query)
+    # raw_query = "INSERT INTO public.feedback (id , date_time, student_id, title, feedback_description) " \
+    #             "VALUES (DEFAULT, NOW()::TIMESTAMP(0), '%s', '%s', '%s')" % (student_id, title, feedback_description)
+    #
+    # print(raw_query)
     with connection.cursor() as cursor:
-        cursor.execute(raw_query)
+        cursor.execute("""
+        INSERT INTO public.feedback (id , date_time, student_id, title, feedback_description) 
+        VALUES (DEFAULT, NOW()::TIMESTAMP(0), %s, %s, %s);
+        """, (student_id, title, feedback_description))
     connection.commit()
 
     return HttpResponse(status=202)
@@ -51,11 +53,10 @@ def add_feedback(request):
 
 def del_feedback(request):
     _id = int(request.POST.get('id', 1))
-    print(_id)
-    raw_query = "DELETE FROM public.feedback WHERE id = %d" % _id
-    print(raw_query)
     with connection.cursor() as cursor:
-        cursor.execute(raw_query)
+        cursor.execute("""
+        DELETE FROM public.feedback WHERE id = %s ;
+        """, (_id,))
     connection.commit()
 
     return HttpResponse(status=200)
