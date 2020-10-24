6ded4ee166333688066db3ad3a5bbfe12b4b8a66
Shubhang Tripathi <stripathi1@cs.iitr.ac.in>
2020-10-24 16:09:56 +0530

    added submodule, deleted original backend file

--- backend/feedback/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from feedback import views
-
-urlpatterns = [
-    path('', views.get_feedback, name='get_feedback'),
-    path('add', views.add_feedback, name='add_feedback'),
-    path('del', views.del_feedback, name='del_feedback'),
-]
--- backend/menu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from menu import views
-
-urlpatterns = [
-    path('', views.MenuByDay.as_view(), name='day'),
-    path('add', views.MenuAdd.as_view(), name='add')
-]
